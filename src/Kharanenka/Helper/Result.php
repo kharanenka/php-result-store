e53be547fd812db2acf3e429b87613bab7162834
Andrey Kharanenka <a.khoronenko@lovata.com>
2016-09-15 21:00:36 +0300

    Develop fasade class

--- src/Kharanenka/Helper/Result.php
+++ src/Kharanenka/Helper/Result.php
@@ -6,106 +6,24 @@
  * Class Result
  * @package Kharanenka\Helper
  * @author Andrey Kharanenka, kharanenka@gmail.com
+ *
+ * @method static ResultStore setTrue(mixed $obData = null)
+ * @method static ResultStore setFalse(mixed $obData = null)
+ * @method static bool result()
+ * @method static mixed data()
+ * @method static array get()
+ * @method static string getJSON()
  */
 
 class Result {
 
-    /**
-     * Status of result (true|false)
-     * @var bool
-     */
-    private $bResult = false;
-
-    /**
-     * Data of result
-     * @var mixed
-     */
-    private $obData;
-
-    /** @var Result */
-    private static $obThis =  null;
-
-    private function __construct() {}
-
-    /**
-     * @return Result
-     */
-    private static function getInstance() {
-
-        if(empty(self::$obThis)) {
-            self::$obThis = new Result();
+    public static function __callStatic($sMethodName, $arArguments)
+    {
+        $obResult = ResultStore::getInstance();
+        if(empty($arArguments)) {
+            return $obResult->$sMethodName();
         }
 
-        return self::$obThis;
-    }
-
-    /**
-     * Set status of result in true
-     * Set data of result
-     * @param mixed $obData
-     * @return Result
-     */
-    public static function setTrue($obData = null) {
-
-        $obThis = self::getInstance();
-        $obThis->bResult  = true;
-        $obThis->obData = $obData;
-        return $obThis;
-    }
-
-    /**
-     * Set status of result in false
-     * Set data of result
-     * @param mixed $obData
-     * @return Result
-     */
-    public static function setFalse($obData = null) {
-
-        $obThis = self::getInstance();
-        $obThis->bResult  = false;
-        $obThis->obData = $obData;
-        return $obThis;
-    }
-
-    /**
-     * @return bool
-     */
-    public static function result() {
-
-        $obThis = self::getInstance();
-        return $obThis->bResult;
-    }
-
-    /**
-     * @return mixed
-     */
-    public static function data() {
-
-        $obThis = self::getInstance();
-        return $obThis->obData;
-    }
-
-    /**
-     * @return array
-     */
-    public static function get() {
-
-        $obThis = self::getInstance();
-
-        return [
-            'result' => $obThis->bResult,
-            'data'   => $obThis->obData,
-        ];
-
-    }
-
-    /**
-     * Generate result JSON string
-     * @return string
-     */
-    public static function getJSON() {
-
-        $obThis = self::getInstance();
-        return json_encode($obThis->get());
+        return call_user_func_array([$obResult, $sMethodName], $arArguments);
     }
 }
